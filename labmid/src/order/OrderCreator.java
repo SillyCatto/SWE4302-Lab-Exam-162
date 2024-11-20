c7b7345f32a3a33adb3bbde94e39de5d4b5e3512
SillyCatto <raiyanmuhtasim@gmail.com>
2024-11-20 15:54:06 +0600

    created order selector

--- labmid/src/order/OrderCreator.java
+++ labmid/src/order/OrderCreator.java
@@ -1,4 +1,29 @@
 package order;
 
+import container.IContainer;
+import flavors.IFlavor;
+import toppings.IToppings;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class OrderCreator {
+    Map<IFlavor,Integer> flavorChoiceAndAmount;
+    Map<IToppings,Integer> toppingChoiceAndAmount;
+    IContainer container;
+
+    public OrderCreator(){
+        flavorChoiceAndAmount = new HashMap<>();
+        toppingChoiceAndAmount = new HashMap<>();
+    }
+
+    public void promptUser(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the flavors you wish to order: ");
+        System.out.println("1. Chocolate Fudge");
+        System.out.println("2. Mint Chocolate Chip");
+        // more options...
+
+    }
 }
---
+++ labmid/src/order/OrderSelector.java
@@ -0,0 +1,23 @@
+package order;
+
+import container.IContainer;
+import flavors.IFlavor;
+import toppings.IToppings;
+
+public class OrderSelector {
+    static IFlavor flavorSelector(){
+        // select order type based on user input
+        return null; // dummy place holder value
+    }
+
+    static IToppings toppingsSelector(){
+        // select toppings type based on user input
+        return null; // dummy place holder value
+    }
+
+    static IContainer containerSelector(){
+        // select container type - paper cup or waffle cone based on
+        // user input
+        return null;
+    }
+}
